eef87171345cad66ba25ae6c5cb55ffb095d1424
JRodriguez-Repo <97987743+JRodriguez-Repo@users.noreply.github.com>
2022-02-15 22:04:54 -0500

    enter

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 $( document ).ready(function() {
     $("#image_slider > div:gt(0)").hide();
 
-setInterval(function() { 
+setInterval( ()=> { 
   $('#image_slider > div:first')
-  .fadeOut(1000)
+  .fadeOut(500)
   .next()
-  .fadeIn(1000)
+  .fadeIn(500)
   .end()
   .appendTo('#image_slider');
 }, 3000);
 
-  $('.services_container').css('display', 'none');
-  $('.services_container').fadeIn(1000);
+$('.services_column').css('display', 'none').fadeIn(2000);
+
 
 
   });
